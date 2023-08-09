478baafaf2d6d4f053f133f8183ed8be809bc126
Isaac-AC <isccds800@gmail.com>
2023-08-08 22:39:43 -0600

    delete version

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, send_file
+import os
+import youtube_dl
+
+app = Flask(__name__)
+
+path = os.getcwd() + '/output/'
+
+@app.route('/')
+def route():
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send():
+    if request.method == 'POST':
+        url = request.form['url']
+
+        ydl_opts = {
+            'format': 'bestvideo/best',
+            'outtmpl': path + '%(title)s.%(ext)s',
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            video_title = info_dict.get('title', '')
+
+        video_path = os.path.join(path, f'{video_title}.mp4')
+        return send_file(video_path, as_attachment=True)
+
+@app.route('/send2', methods=['POST'])
+def send2():
+    if request.method == 'POST':
+        
+        url = request.form['url']
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': path + '%(title)s.%(ext)s',
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            video_title = info_dict.get('title', '')
+
+        audio_path = os.path.join(path, f'{video_title}.mp3')
+        return send_file(audio_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='localhost')
--- video.py
+++
@@ -1,7 +0,0 @@
-from pafy import new
-
-url = input("Enter the url: ")
-
-video = new(url)
-dl = video.getbest()
-dl.download()
