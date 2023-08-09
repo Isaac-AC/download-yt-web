e8a83196efd6f04acf1cb2e80bdd2725eb9c7e62
Isaac-AC <isccds800@gmail.com>
2023-08-09 17:55:38 -0600

    fix audio in video

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import youtube_dl
 
 app = Flask(__name__)
 
-path = os.getcwd() + '/output/'
+
+downloads_path = os.path.join(os.getcwd(), 'Download')
 
 @app.route('/')
 def route():
@@ -16,33 +17,32 @@ def send():
         url = request.form['url']
 
         ydl_opts = {
-            'format': 'bestvideo/best',
-            'outtmpl': path + '%(title)s.%(ext)s',
+            'format': 'best',
+            'outtmpl': os.path.join(downloads_path, '%(title)s.%(ext)s'),
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             video_title = info_dict.get('title', '')
 
-        video_path = os.path.join(path, f'{video_title}.mp4')
+        video_path = os.path.join(downloads_path, f'{video_title}.mp4')
         return send_file(video_path, as_attachment=True)
 
 @app.route('/send2', methods=['POST'])
 def send2():
     if request.method == 'POST':
-        
         url = request.form['url']
 
         ydl_opts = {
             'format': 'bestaudio/best',
-            'outtmpl': path + '%(title)s.%(ext)s',
+            'outtmpl': os.path.join(downloads_path, '%(title)s.%(ext)s'),
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             video_title = info_dict.get('title', '')
 
-        audio_path = os.path.join(path, f'{video_title}.mp3')
+        audio_path = os.path.join(downloads_path, f'{video_title}.mp3')
         return send_file(audio_path, as_attachment=True)
 
 if __name__ == '__main__':
